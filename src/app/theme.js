8b7e2e407a5ebcac7c216c396c48f2bea0565646
woooyoung <axdsw121@gmail.com>
2025-02-07 15:46:51 +0900

    Todo 추가 폼 꾸미기, TextField, Button

--- src/app/theme.js
+++ src/app/theme.js
@@ -1,13 +1,16 @@
 import { createTheme } from '@mui/material';
 
 const theme = createTheme({
+  typography: {
+    fontFamily: 'GmarketSansMedium',
+  },
   palette: {
     type: 'light',
     primary: {
-      main: '#f50057',
+      main: '#f44336',
     },
     secondary: {
-      main: '#f44336',
+      main: '#f50057',
     },
   },
 });
